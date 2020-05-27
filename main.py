a05368abe2bfe5e953a06501afef2ea8a94e3db8
Owolabi Oromidayo <owolabi.oromidayo@stu.cu.edu.ng>
2020-05-27 23:01:12 +0100

    Dynamic window creation and entry

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-import tkinter as tk
 import random
+import itertools
+import tkinter as tk
 from tkinter import messagebox
+import numpy as np
 
 
 class TicTacToe:
-    def __init__(self, size):
+    def __init__(self, size, cell_size):
         # Initial game state
-        self.size = size
-        self.choices = list(range(0, self.size**2))  # board postions
+        self.size, self.cell_size = size, cell_size
+        self.choices = set(range(0, self.size**2))  # board postions
         self.player_turn = True
         self.game_over = False
 
@@ -15,52 +17,91 @@ class TicTacToe:
         self.game = [{i: None} for i in range(self.size**2)]
         # Groups list of dicts into groups of 3
         iters = [iter(self.game)] * self.size
+        # iters = list(map(lambda x: list(x), iters))
+
+        # print(iters)
         self.game = list(zip(*iters))
+        self.game = list(map(lambda x: list(x), self.game))
         print(self.game)
 
+        # self._test_space()
         self.build()
 
+    def gen_centers(self):
+        # x_edges = np.uniform(0, self.size*100, self.size)
+        # edges = [100*i for i in range(self.size+1)]
+        # print(edges)
+        # centers = []
+        # for idx, i in enumerate(edges[:-1]):
+        #     new_center = (i + edges[idx+1])/2
+        #     centers.append(new_center)
+
+        self.centers_1d = [self.cell_size/2+(self.cell_size*i)
+                           for i in range(self.size)]
+
+        # print(centers)
+        # pairs = []
+        # for i in centers:
+        #     new_row = [[i, j] for j in centers]
+        #     for pos in new_row:
+        #         pairs.append(pos)
+
+        self.centers = [[x, y]
+                        for x in self.centers_1d for y in self.centers_1d]
+
+    def gen_lines(self, linewidth):
+        # Doesnt include first and last, only ones necceaasry for line to cross
+        self.edges = [self.cell_size*i for i in range(1, self.size)]
+
+        for edge in self.edges:
+            self.canvas.create_line(
+                edge, 0, edge, self.height, width=linewidth
+            )  # vert lines
+
+            self.canvas.create_line(
+                0, edge, self.width, edge, width=linewidth
+            )  # horiz lines
+
+        # self.vert1 = self.canvas.create_line(
+        #     self.width/3, 0, self.width/3, self.height, width=linewidth)
+        # self.vert2 = self.canvas.create_line(
+        #     self.width*2/3, 0, self.width*2/3, self.height, width=linewidth)
+
+        # self.horiz1 = self.canvas.create_line(
+        #     0, self.height/3, self.width, self.height/3, width=linewidth)
+        # self.horiz2 = self.canvas.create_line(
+        #     0, self.height*2/3, self.width, self.height*2/3, width=linewidth)
+
     def build(self):
-        self.width, self.height = 800, 800
+        self.width = self.height = self.size*self.cell_size
+
         self.root = tk.Tk()
         self.root.title("Tic-Tac-Toe")
         self.root.geometry(f"{self.width}x{self.height}")
         self.root.bind('<Button-1>', self.on_click)
 
-        self.centers = ([133, 133], [400, 133], [667, 133],
-                        [133, 400], [400, 400], [667, 400],
-                        [133, 667], [400, 667], [667, 667]
-                        )
+        self.gen_centers()
+
         # Create Board
         self.canvas = tk.Canvas(
             self.root, width=self.width, height=self.height)
         self.canvas.pack()
 
-        # Create Lines
-        self.linewidth = 7
-        self.vert1 = self.canvas.create_line(
-            self.width/3, 0, self.width/3, self.height, width=self.linewidth)
-        self.vert2 = self.canvas.create_line(
-            self.width*2/3, 0, self.width*2/3, self.height, width=self.linewidth)
-
-        self.horiz1 = self.canvas.create_line(
-            0, self.height/3, self.width, self.height/3, width=self.linewidth)
-        self.horiz2 = self.canvas.create_line(
-            0, self.height*2/3, self.width, self.height*2/3, width=self.linewidth)
+        self.gen_lines(7)
         self.root.mainloop()
 
     def on_click(self, event):
-        if self.player_turn == True:
-            x, y = event.x, event.y
-            self.position = self.get_position(x, y)
-            if self.position in self.choices:
-                self.draw('X', self.position)
-                self.choices.remove(self.position)
-                self.game[self.position] = 'X'
-                self.check_game_over()
-                self.player_turn = False
+        if self.player_turn:
+            pos, x_pos, y_pos = self.get_pos(event.x, event.y)
+            if pos in self.choices:
+                self.draw('X', pos)
+                self.choices.remove(pos)
+                self.game[y_pos][x_pos*(y_pos+1)] = {pos: 'X'}
+                print(self.game)
+                # self.check_game_over()
+                # self.player_turn = False
 
-                self.computer_choice()
+                # self.computer_choice()
         else:
             pass
 
@@ -73,9 +114,8 @@ class TicTacToe:
         self.check_game_over()
         self.player_turn = True
 
-    def draw(self, letter, position):
-        x = self.centers[position][0]
-        y = self.centers[position][1]
+    def draw(self, letter, pos):
+        x, y = self.centers[pos][1], self.centers[pos][0]
         color = 'blue' if letter == 'O' else 'red'
         self.canvas.create_text(
             x, y, text=letter, font=('Times', 44), fill=color)
@@ -109,17 +149,15 @@ class TicTacToe:
         messagebox.showinfo(title='Game Over', message=message)
         self.root.destroy()
 
-    def get_position(self, x, y):
+    def get_pos(self, x, y):
         """returns board position based on x and y coords"""
-        for i in range(1, 4):
-            _max = 267*i
-            if x < _max:
-                if y > 534:
-                    return i+5
-                elif y > 267:
-                    return i+2
-                else:
-                    return i-1
+        x_pos, y_pos = 0, 0
+        for edge in self.edges:
+            x_pos = x_pos+1 if x > edge else x_pos
+            y_pos = y_pos+1 if y > edge else y_pos
+
+        pos = y_pos*self.size + x_pos
+        return pos, x_pos, y_pos
 
 
-ttc = TicTacToe(5)
+ttc = TicTacToe(5, 100)
